daebb7b829bf8611102797335bb8a39c8f8a5b25
chae-young <cycy8527@gmail.com>
2019-12-27 16:53:05 +0900

    update

--- project/Rock_Paper_Scissors/Rock_Paper_Scissors/js/rock_paper_scissors2.js
+++ project/Rock_Paper_Scissors/Rock_Paper_Scissors/js/rock_paper_scissors2.js
@@ -12,8 +12,9 @@ introEffect();
 
 var computer = document.getElementById('computer');
 var me = document.getElementById('me');
+var btnBox = document.querySelector('.rps--bottom');
 
-var rps = ['images/noun_rock.png','images/noun_paper.png','images/noun_scissors.png']
+var rps = ['images/noun_scissors.png','images/noun_rock.png','images/noun_paper.png']
 
 var radio = document.querySelectorAll('input');
 var round = 0;
@@ -32,7 +33,7 @@ function computerInterval(){
 			computerImg = 0;
 		}
 		computer.innerHTML = "<img src='" + rps[computerImg] + "' alt=''>";	
-	},200)
+	},100)
 }
 
 var interval2;
@@ -46,75 +47,82 @@ function meInterval(){
 			meImg = 0;
 		}
 		me.innerHTML = "<img src='" + rps[meImg] + "' alt=''>";	
-	},200)
+	},100)
 }
 
 //var radioArea = document.querySelector('.radio-area').classList.add('active')
+var result = document.getElementById('result');
+var computerNowScore = 0;
+var myNowScore = 0;
 
-radio.forEach(function(el){
+Array.prototype.slice.call(radio).forEach(function(el){
 	el.addEventListener('change',function(){
 		el.checked = true;
 		round = el.value;
-		console.log('라운드 : ' ,round)
+		//console.log('라운드 : ' ,round)
 		
 		setTimeout(function(){
 			document.querySelector('.rps-area').classList.add('active');
 			document.querySelector('.radio-area').classList.remove('active');
 			
 		},800)
-
+		
 		//var radioArea = document.querySelector('.rps-area').classList.add('active')
 		computerInterval()
 		meInterval()
+		btnBox.classList.add('on');
+	})
+})
 
-		var computerTxt = document.getElementById('computerNowScore');
-		var myTxt = document.getElementById('myNowScore');
-		var computerNowScore = 0;
-		var myNowScore = 0;
-		var btn = document.querySelectorAll('.btn');
-		btn.forEach(function(btn,idx){
-			btn.addEventListener('click',function(){
-				me.innerHTML = "<img src='" + rps[idx] + "' alt=''>";	
-				clearInterval(interval)
-				clearInterval(interval2)
-				round--
+var computerTxt = document.getElementById('computerNowScore');
+var myTxt = document.getElementById('myNowScore');
+var btn = document.querySelectorAll('.btn');
+btn.forEach(function(btn,idx){
+	console.log(btn)
+	btn.addEventListener('click',function(){
 
-				var score = idx - computerImg;
-				//console.log(btn,'라운드',round,'컴퓨터점수',computerNowScore, '나의점수',myNowScore,'현재점수차',score);
-				if(score == 0){
-					console.log('비겼어요')
-				}else if([-1,2].includes(score)){
-					console.log('졌어요')			
-					computerNowScore = computerNowScore + 1;
-					computerTxt.textContent = computerNowScore;				
+		me.innerHTML = "<img src='" + rps[idx] + "' alt=''>";	
+		clearInterval(interval)
+		clearInterval(interval2)
+		round--
+
+		var score = idx - computerImg;
+
+		//console.log(btn,'라운드',round,'컴퓨터점수',computerNowScore, '나의점수',myNowScore,'현재점수차',score);
+		if(score == 0){
+			console.log('비겼어요')
+		}else if([-1,2].includes(score)){
+			console.log('졌어요')			
+			computerNowScore = computerNowScore + 1;
+			computerTxt.textContent = computerNowScore;				
+		}else{
+			console.log('이겼어요')		
+			myNowScore = myNowScore + 1;
+			myTxt.textContent = myNowScore;				
+		}
+		setTimeout(function(){
+			computerInterval()
+			meInterval()
+
+			//가위바위보가 다 끝나후,승패결정
+			if(1 > round){
+				clearInterval(interval);
+				clearInterval(interval2);
+				btnBox.classList.remove('on');
+				radio.forEach(function(el){			
+					el.checked = false;
+				})
+
+				if(computerNowScore > myNowScore){
+					result.textContent = '컴퓨터의 승!'
+				}else if(computerNowScore < myNowScore){
+					result.textContent = '나의 승!'
 				}else{
-					console.log('이겼어요')		
-					myNowScore = myNowScore + 1;
-					myTxt.textContent = myNowScore;				
+					result.textContent = '비겼습니다.'
 				}
-				setTimeout(function(){
-					computerInterval()
-					meInterval()
+			}
+		},1000)
 
-					//가위바위보가 다 끝나후,승패결정
-					if(1 > round){
-						clearInterval(interval);
-						clearInterval(interval2);
-						el.checked = false;
-
-						var result = document.getElementById('result');
-						if(computerNowScore > myNowScore){
-							result.textContent = '컴퓨터의 승!'
-						}else if(computerNowScore < myNowScore){
-							result.textContent = '나의 승!'
-						}else{
-							result.textContent = '비겼습니다.'
-						}
-					}
-				},1000)
-
-			})
-		})
 	})
 })
 
@@ -125,10 +133,17 @@ document.getElementById('resetBtn').addEventListener('click',function(){
 		el.classList.remove('active');
 	})
 	introEffect();
+
 	//모두리셋	
 	computer.innerHTML = '';
 	me.innerHTML = '';
+	result.textContent = "";
+	computerNowScore = 0;
+	myNowScore = 0;
+	myTxt.textContent = "0";
+	computerTxt.textContent = "0";
 })
+
 
 
 //나 컴퓨터
