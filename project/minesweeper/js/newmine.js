8ef2b8f40c3fd63945ac013fbe6ee4a5e32ec5fd
chae-young <cycy8527@gmail.com>
2020-04-03 18:19:33 +0900

    update

--- project/minesweeper/js/newmine.js
+++ project/minesweeper/js/newmine.js
@@ -4,11 +4,15 @@
 1.실행 클릭했을때 값을 입력받아 지뢰데이터,지뢰테이블만들기
 2.지뢰갯수만큼 지뢰를 랜덤으로심기
 3.지뢰가 아닌곳을 클릭했을때 지뢰가 몇개인지 주변칸에서 표시
+4.지뢰인곳을 오른쪽클릭했을때 느낌표 물음표 설정. 
+5.지뢰인곳클릭하면 팡!
+6.0인곳을 클릭하면 옆에칸이 열어지도록
 */
 
 var datamine = [];
 var tbody = document.querySelector('#table tbody');
 var x = 'x';
+var flag = false;
 var datatable = {
 
 }
@@ -16,6 +20,7 @@ var datatable = {
 document.getElementById('exec').addEventListener('click',function(){
 	tbody.innerHTML = '';
 	datamine = [];
+	flag = false;
 
 	var hor = parseInt(document.getElementById('hor').value);
 	var ver = parseInt(document.getElementById('ver').value);
@@ -31,14 +36,52 @@ document.getElementById('exec').addEventListener('click',function(){
 			var td = document.createElement('td');
 			//칸 클릭.
 			td.addEventListener('click',function(e){
+				if(flag){
+					return;
+				}
 				//클릭한칸이 몇번째 칸이고 몇번째 줄인지
-				var thisparent = e.currentTarget.parentNode;
-				var thisTopparent = e.currentTarget.parentNode.parentNode;
-				var thisLine = Array.prototype.indexOf.call(thisTopparent.children,thisparent)
-				var thisBox = Array.prototype.indexOf.call(thisparent.children,e.currentTarget)
+				var thisparent = e.currentTarget.parentNode;//클릭한칸의 부모
+				var thisTopparent = e.currentTarget.parentNode.parentNode;//클릭한칸의 최상위부모
+				var thisLine = Array.prototype.indexOf.call(thisTopparent.children,thisparent);//클릭한 줄
+				var thisBox = Array.prototype.indexOf.call(thisparent.children,e.currentTarget);//클릭한 칸
 				
-				var aroundBox = [];
-				console.log(datamine[thisLine][thisBox])
+
+				var aroundBox = [datamine[thisLine][thisBox-1],datamine[thisLine][thisBox+1]];
+				if(datamine[thisLine-1]){
+					aroundBox = aroundBox.concat( [datamine[thisLine-1][thisBox-1],datamine[thisLine-1][thisBox],datamine[thisLine-1][thisBox+1]] )
+				}
+				if(datamine[thisLine+1]){
+					aroundBox = aroundBox.concat( [datamine[thisLine+1][thisBox-1],datamine[thisLine+1][thisBox],datamine[thisLine+1][thisBox+1]] )
+				}
+				var aroundnum = e.currentTarget.textContent = aroundBox.filter(function(v){
+					return v == x;
+				}).length
+				
+				//주변지뢰열기
+				if(aroundnum == 0){
+					
+				}
+
+				//지뢰면
+				if(datamine[thisLine][thisBox] == x){
+					e.currentTarget.textContent = '팡'
+					flag = true;
+				}
+			})
+			td.addEventListener('contextmenu',function(e){
+				e.preventDefault();
+				var thisparent = e.currentTarget.parentNode;//클릭한칸의 부모
+				var thisTopparent = e.currentTarget.parentNode.parentNode;//클릭한칸의 최상위부모
+				var thisLine = Array.prototype.indexOf.call(thisTopparent.children,thisparent);//클릭한 줄
+				var thisBox = Array.prototype.indexOf.call(thisparent.children,e.currentTarget);//클릭한 칸
+
+				if(e.currentTarget.textContent == x){
+					e.currentTarget.textContent = '!';
+				}else if(e.currentTarget.textContent == '!'){
+					e.currentTarget.textContent = '?';
+				}else if(e.currentTarget.textContent == '?'){
+					e.currentTarget.textContent = x;
+				}
 			})
 			tr.appendChild(td);
 			arr.push(0)
