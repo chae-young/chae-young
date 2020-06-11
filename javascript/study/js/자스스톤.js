840b756c224183e0a1159892141021867f091ad4
chae-young <cycy8527@gmail.com>
2020-06-11 21:57:40 +0900

    add:new

--- javascript/study/js/자스스톤.js
+++ javascript/study/js/자스스톤.js
@@ -51,7 +51,20 @@ function 덱에서필드로(데이터,내턴){//데이터가 없기때문에 돔
     나.코스트.textContent = 현재코스트 - 데이터.cost;
 
 }
-
+//상대인지아닌지 확인후 다시그린다.
+function 화면다시그리기(내화면){//내화면 true 상대 false
+    var 객체 = 내화면 ? 나 : 상대;
+    객체.덱.innerHTML = '';
+    객체.필드.innerHTML = '';
+    객체.영웅.innerHTML = '';
+    객체.필드data.forEach(function(data){//다시그리기
+        카드돔연결(data,객체.필드)
+    })
+    객체.덱data.forEach(function(data){
+        카드돔연결(data,객체.덱)
+    }) 
+    카드돔연결(객체.영웅data,객체.영웅,true)
+}
 function 카드돔연결(데이터,돔,영웅){
     var 카드 = document.querySelector('.card-hidden .card').cloneNode(true);
     카드.querySelector('.card-cost').textContent = 데이터.cost;
@@ -65,16 +78,33 @@ function 카드돔연결(데이터,돔,영웅){
     }
 
     카드.addEventListener('click',function(card){
+        console.log(데이터.mine)
         if(턴){// 내턴이면
-            if(!데이터.mine){//상대카드고 내카드가 선택되어있으면 공격
-                데이터.hp = 데이터.hp - 나.선택카드.att //상대방체력에 내 공격력을 뺌
+            if(!카드.classList.contains('card-turnover')){//턴오버된거는 안되용{
+                console.log(!카드.classList.contains('card-turnover'))
+                //return;
+            }
+            if(!데이터.mine && 나.선택카드){//상대카드면서 내카드가 선택되어 있고
+                
+                데이터.hp = 데이터.hp - 나.선택카드data.att //상대방체력에 내 공격력을 뺌
+                if(데이터.hp <= 0){//카드가 죽었을때
+                    //데이터를제거
+                    var 인덱스 = 상대.필드data.indexOf(데이터);
+                    if(인덱스 > -1){//쫄병이 죽었을때
+                        상대.필드data.splice(인덱스,-1);//잘라내기
+                    }else{//영웅이 죽었을떄
+                        alert('승리했다')
+                        초기세팅()
+                    }
+                }
+                화면다시그리기(false)//상대화면
                 나.선택카드.classList.remove('card-selected');
                 나.선택카드.classList.add('card-turnover');
                 나.선택카드 = null;//내가 선택한 카드 해제
                 나.선택카드data = null;
                 return;
             }else if(!데이터.mine){//상대카드면
-
+                return
             }
             if(데이터.field){//카드가 필드에 있으면
                 //card-selec을 모두 제거하고 선택한 것만 추가
@@ -92,12 +122,45 @@ function 카드돔연결(데이터,돔,영웅){
                 }
             }
         }else{//상대방
-            if(데이터.mine || 데이터.field){//내카드라면
-                return;
-            }                   
-            if(!덱에서필드로(데이터,false)){
-                상대덱생성(1);
+            if(!카드.classList.contains('card-turnover')){//턴이끝난카드는 아무일도 일어나지 않음
+                console.log(!카드.classList.contains('card-turnover'))
+                //return;
             }
+            if(데이터.mine && 상대.선택카드){//내카드면서 상대카드가 선택되어 있으면 공격
+                
+                데이터.hp = 데이터.hp - 상대.선택카드data.att //상대방체력에 내 공격력을 뺌
+                if(데이터.hp <= 0){//카드가 죽었을때
+                    //데이터를제거
+                    var 인덱스 = 나.필드data.indexOf(데이터);
+                    if(인덱스 > -1){//쫄병이 죽었을때
+                        나.필드data.splice(인덱스,-1);//잘라내기
+                    }else{//영웅이 죽었을떄
+                        alert('패배했다')
+                        초기세팅();
+                    }
+                }                
+                화면다시그리기(true)//내화면
+                상대.선택카드.classList.remove('card-selected');
+                상대.선택카드.classList.add('card-turnover');
+                상대.선택카드 = null;//내가 선택한 카드 해제
+                상대.선택카드data = null;
+                return;
+            }else if(데이터.mine){
+                return
+            }
+            if(데이터.field){
+                카드.parentNode.querySelectorAll('.card').forEach(function(card){
+                    card.classList.remove('card-selected')
+                })
+                카드.classList.add('card-selected');
+                상대.선택카드 = 카드//내가선택한 카드를 변수에 저장
+                상대.선택카드data = 데이터
+            }else{
+                
+                if(!덱에서필드로(데이터,false)){
+                    상대덱생성(1);
+                }
+            }               
         }
     })
 
@@ -142,6 +205,7 @@ function Card(영웅,내카드){
         this.att = Math.floor(Math.random() * 5);
         this.hp = Math.floor(Math.random() * 5) + 25;//체력업!   
         this.hero = true;     
+        this.field = true;//영웅은 첨부터 필드에
     }else{
         this.att = Math.floor(Math.random() * 5);
         this.hp = Math.floor(Math.random() * 5);
@@ -161,10 +225,22 @@ function 초기세팅(){
     내덱생성(5);
     내영웅생성();
     상대영웅생성();   
+    화면다시그리기(true);
+    화면다시그리기(false);
 }
 
 턴버튼.addEventListener('click',function(){
-    턴 = !턴;
+    //턴을 넘기기전에 턴오버된것을 다시그려줌
+    var 객체 = 턴 ? 나 : 상대;
+    console.log(객체)
+    객체.필드.innerHTML = '';
+    객체.영웅.innerHTML = '';
+    객체.필드data.forEach(function(data){//다시그리기
+        카드돔연결(data,객체.필드)
+    })
+    카드돔연결(객체.영웅data,객체.영웅,true)  
+
+    턴 = !턴; // 턴을넘김
     if(턴){//턴 넘길때마다 다시 10으로 채워줌
         나.코스트.textContent = 10;
     }else{
@@ -172,6 +248,8 @@ function 초기세팅(){
     }
     document.getElementById('rival').classList.toggle('turn');
     document.getElementById('my').classList.toggle('turn');
+    //
+  
 });
 
 초기세팅();
