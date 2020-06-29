741540e59d36f261b6f51a5af0bcec31e2a62e5b
chae-young <cycy8527@gmail.com>
2020-06-29 18:21:50 +0900

    js_add_youtube

--- slick_api/youtube/youtube.js
+++ slick_api/youtube/youtube.js
@@ -7,6 +7,7 @@
 	  autoplaySpeed: 2000,
 	});
 
+  var basicSpeed = slider.get(0).slick.options.autoplaySpeed;
   // 2. This code loads the IFrame Player API code asynchronously.
   var tag = document.createElement('script');
 
@@ -17,36 +18,59 @@
   // 3. This function creates an <iframe> (and YouTube player)
   //    after the API code downloads.
   var player;
+ 
   function onYouTubeIframeAPIReady() {
-	player = new YT.Player('player', {
-	  events: {
-		'onReady': onPlayerReady,
-		'onStateChange': onPlayerStateChange
-	  }
+	
+	function playerSet(name){
+		player = new YT.Player(name, {
+		  events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		  }
+		});
+
+	}
+	playerSet('player1')
+	console.log('2')
+
+
+
+	 slider.on('afterChange', function(event, slick, currentSlide) {
+		console.log('변경후');
+		playerSet('player2')
+		slider.slick('slickSetOption', 'autoplaySpeed', basicSpeed);
 	});
+
   }
 
   // 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-	player.mute()
-	player.pauseVideo()
-	//event.target.playVideo();
+  function onPlayerReady(event) { //처음 로드시
+	player.mute()//유튜브음소거 필수
+	
+	console.log('3')
+	  if(slider.find('.slick-current iframe').length){
+		slider.slick('slickPause');
+		player.playVideo();
+	  }
+	 slider.on('beforeChange', function(event, slick, currentSlide) {
+		console.log('변경전');
+		player.pauseVideo();
+	 });
   }
 
   function onPlayerStateChange(event) {
-	console.log(event.data)
-	if(event.data == 1){
-		slider.slick('slickPause');
+	switch(event.data){
+		case 1:
+		console.log('시작');
+		break;
+		case 0:
+		console.log('끝');
+		slider.slick('slickSetOption', 'autoplaySpeed', 0);
+		slider.slick('slickPlay');
+		break;
 	}
   }
-  function stopVideo() {
 
-  }
 
- slider.on('beforeChange', function(event, slick, currentSlide) {
-	
-});
- slider.on('afterChange', function(event, slick, currentSlide) {
-    player.playVideo()
 
-});
+console.log('1')
