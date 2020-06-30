9c825c9eff1f676b3de1cc8cbbfa3b79cf63a6eb
chae-young <cycy8527@gmail.com>
2020-06-30 18:30:14 +0900

    js_add

--- slick_api/youtube/youtube.js
+++ slick_api/youtube/youtube.js
@@ -3,65 +3,46 @@
 	slider.slick({
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
-	  autoplay: true,
+	  //autoplay: true,
 	  autoplaySpeed: 2000,
 	});
 
   var basicSpeed = slider.get(0).slick.options.autoplaySpeed;
-  // 2. This code loads the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
 
+  var tag = document.createElement('script');
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
   var player;
- 
-  function onYouTubeIframeAPIReady() {
-	
-	function playerSet(name){
+  function onYouTubeIframeAPIReady(name) {
+	console.log('첫로드불러오기')
+	if(slider.find('.slick-current iframe').length){
+	    name = slider.find('.slick-current iframe').attr('id');
 		player = new YT.Player(name, {
 		  events: {
 			'onReady': onPlayerReady,
 			'onStateChange': onPlayerStateChange
 		  }
 		});
-
 	}
-	playerSet('player1')
-	console.log('2')
-
-
-
-	 slider.on('afterChange', function(event, slick, currentSlide) {
-		console.log('변경후');
-		playerSet('player2')
-		slider.slick('slickSetOption', 'autoplaySpeed', basicSpeed);
-	});
-
   }
 
   // 4. The API will call this function when the video player is ready.
   function onPlayerReady(event) { //처음 로드시
 	player.mute()//유튜브음소거 필수
-	
-	console.log('3')
-	  if(slider.find('.slick-current iframe').length){
-		slider.slick('slickPause');
-		player.playVideo();
-	  }
-	 slider.on('beforeChange', function(event, slick, currentSlide) {
-		console.log('변경전');
-		player.pauseVideo();
-	 });
+	console.log('로드검열')
+	slider.slick('slickPause');
+	player.playVideo();
   }
 
   function onPlayerStateChange(event) {
 	switch(event.data){
 		case 1:
 		console.log('시작');
+		break;
+		case 2:
+		console.log('정지상태')
 		break;
 		case 0:
 		console.log('끝');
@@ -71,6 +52,17 @@
 	}
   }
 
+slider.on('beforeChange', function(event, slick, currentSlide) {
+	if(slider.find('.slick-current iframe').length){
+		player.pauseVideo();	
+	}
+})
+slider.on('afterChange', function(event, slick, currentSlide) {
+	console.log('변경후')
+	slider.slick('slickSetOption', 'autoplaySpeed', basicSpeed);
 
-
-console.log('1')
+	if( $(slick.$slides.get(currentSlide)).find('iframe').length ){
+		onYouTubeIframeAPIReady();
+		console.log(player)
+	}
+})
