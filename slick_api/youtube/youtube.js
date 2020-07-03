01040d78d298ac6ebbd5052c03f4e63904925107
chae-young <cycy8527@gmail.com>
2020-07-03 18:06:23 +0900

    add_js

--- slick_api/youtube/youtube.js
+++ slick_api/youtube/youtube.js
@@ -1,11 +1,11 @@
-	var slider = $('.autoplay');
+  var slider = $('.autoplay');
 
-	slider.slick({
+   slider.slick({
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
-	  //autoplay: true,
+	  autoplay: true,
 	  autoplaySpeed: 2000,
-	});
+   });
 	
   var basicSpeed = slider.get(0).slick.options.autoplaySpeed;
 
@@ -15,12 +15,12 @@
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
   var player;
-  var num = 0;
-  var name;
-  var done;
+  var n = 0;
+  var playerOB = {};
 
-  function onYouTubeIframeAPIReady() {
-	done = false;
+
+function onYouTubeIframeAPIReady() {
+
 	if(slider.find('.slick-current iframe').length){
 		
 		//var id = slider.find('.slick-current iframe').attr('id');
@@ -30,69 +30,78 @@
 	    name = slider.find('.slick-current iframe').attr('id');
 		player = new YT.Player(name, {
 		  events: {
-			'onReady': onPlayerReady,
-			'onStateChange': onPlayerStateChange
+			onReady: onPlayerReady,
+			onStateChange: onPlayerStateChange
 		  },
 		})
 	}
+
 }
 
 
   // 4. The API will call this function when the video player is ready.
-
-  function onPlayerReady(event) { //처음 로드시
+var et 
+function onPlayerReady(event) { //처음 로드시
+	console.log(event.target.getVideoData())
+	et = event.target;
 	console.log('로드검열')
+	playerOB[$(player.f).attr('id')] = 1;
 	player.mute()//유튜브음소거 필수
-	//console.log('로드검열')
-	//slider.slick('slickPause');
+
+	slider.slick('slickPause');
 	event.target.playVideo();
-  }
+	//playerOB.name+n = '1';
+}
 
-  var playerState;
+var playerState;
 
-  function onPlayerStateChange(event) {
-	console.log('재생표시')
+function onPlayerStateChange(event) {
+	console.log('재생상태',event.data,event.target)
 	switch(event.data){
 		case 1:
-		console.log(player,'재생');
+		//console.log('재생표시','재생');
+			slider.slick('slickPause');
 		break;
 
 		case 2:
-		console.log(player,'정지상태')
-			//player.playVideo();
+		//console.log('재생표시','정지상태')
+			slider.slick('slickPause');
 		break;
 
 		case 0:
+			slider.slick('slickSetOption', 'autoplaySpeed', basicSpeed);
+			slider.slick('slickPlay');
 		break;
-		slider.slick('slickPlay');
 	}
 	playerState = event.data;
-	done = true;
 
-  }
-
+}
 
 
 slider.on('beforeChange', function(event, slick, currentSlide) {
 	console.log('슬릭비포')
-	//if( $(slick.$slides.get(currentSlide)).find('iframe').length ){
+	if( $(slick.$slides.get(currentSlide)).find('iframe').length ){
+	    playerOB[$(player.f).attr('id')] = 2;
 		if(playerState == 1){
-			player.pauseVideo();
+			et.pauseVideo();
 		}
-
-	//}	
+	}
 })
 slider.on('afterChange', function(event, slick, currentSlide) {
 	console.log('슬릭애프터')
 	if( $(slick.$slides.get(currentSlide)).find('iframe').length ){
 		onYouTubeIframeAPIReady()
-			done = true
-			console.log(done)
-		if(done){
-			if(playerState == 2){
-				console.log('222')
-				player.playVideo();
+
+			if( playerOB[$(player.f).attr('id')] == 2 ){
+					et.f = player.f;
+
+				if(playerState == YT.PlayerState.PAUSED || playerState == YT.PlayerState.ENDED){
+					et.playVideo()
+					console.log(playerState,'현재 재생상태')
+				}
 			}
-		}
+
 	}	
 })
+
+//로드검열중일때는 재생되면 안됨.
