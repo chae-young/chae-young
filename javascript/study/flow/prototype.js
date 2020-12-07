07340284303d9cb8c656ed4f97697a6fd22db26a
chae-young <cycy8527@gmail.com>
2020-12-07 21:21:03 +0900

    prototype_add_method

--- javascript/study/flow/prototype.js
+++ javascript/study/flow/prototype.js
@@ -5,10 +5,48 @@
 __proto__ 생략가능.
 
 
-constructor prototype
-
-instance
 
 
+*/
+
+function Person(n,a){
+    this.name = n;
+    this.age = a;
+}
+var gomu = new Person('고무',30);
+var iu = new Person('아이유',30);
+
+/*
+gomu.setOlder = function(){
+    this.age += 1;
+}
+gomu.getAge = function(){
+    return this.age;
+}
+iu.setOlder = function(){
+    this.age += 1;
+}
+iu.getAge = function(){
+    return this.age;
+}
+*/
+Person.prototype.setOlder = function(){
+    this.age += 1;
+}
+Person.prototype.getAge = function(){
+    return this.age;
+}
+
+
+// gomu.setOlder() 로 불러오면 .앞이 this 이기 때문에 불러온다
+/*
+
+gomu.__proto__.setOlder()로 불러올경우
+앞에 this가 프로토가됨.
+프로토는 생략가능!!!!
+
+ */
+
+ /*
 
 */
