5f2905a000afacf84d107feb0538231d981a469e
chae-young <cycy8527@gmail.com>
2020-12-14 22:55:10 +0900

    template_문자열

--- es6/bsv2.js
+++ es6/bsv2.js
@@ -3,11 +3,10 @@
 let funcs = [];
 for(let i = 0; i < 10; i++){
     funcs.push(function(){ //배열에 함수 추가할 경우
-        console.log(i)
+        //console.log(i)
     })
 }
 funcs.forEach(function(f){
     f() //함수실행
 })
 
-console.log(funcs)
