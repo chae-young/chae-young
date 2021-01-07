2c770c11cd2a24e706b295f66fc404135294201d
chae-young <cycy8527@gmail.com>
2021-01-07 21:44:56 +0900

    할당

--- es6/function.js
+++ es6/function.js
@@ -1,6 +1,7 @@
 //new target ->new 함수가 붙은게 타겟
 
-
+/*
+ES5
 function Person (name){
     console.log(this)
     if(this instanceof Person){//this가 person의 인스턴스이면
@@ -10,3 +11,12 @@ function Person (name){
     }
 }
 console.log(new Person())
+*/
+
+function Person(name){
+    if(new.target !== undefined){
+        this.name = name;
+    }else{
+        throw new Error('new 연산자를 사용하세요')
+    }
+}
