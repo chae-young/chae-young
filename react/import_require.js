a8355822cfcf4e178c22dc96ce4d0e1449936e62
chae-young <cycy8527@gmail.com>
2021-03-07 00:37:14 +0900

    import_

---
+++ react/import_require.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import ReactDom from 'react-dom';
+//const {Component} = React
+
+//wordrealy.jsx 가져오기
+import WordRelay from './WordRealy'
+
+
+//module.exports = GuGuDan
+export default GuGuDan
