f4850a2f7c376e38a22d3f1be9289726e303616d
chae-young <cycy8527@gmail.com>
2021-03-19 10:30:52 +0900

    copy_directory_r

--- react/baseball/client.jsx
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const NumberBaseball = require('./baseBall')
-
-ReactDOM.render(<NumberBaseball/>,document.querySelector('#root'))
--- react/endword/client.jsx
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-//wordrealy.jsx 가져오기
-const WordRelay = require('./WordRealy')
-
-ReactDom.render(<WordRelay/>,document.querySelector('#root'))
